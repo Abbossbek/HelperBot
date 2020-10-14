ee754cd27a15d48fe535f9561c7ccda092d0ca57
Abbosbek <39489742+Abbossbek@users.noreply.github.com>
2020-10-14 13:22:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('??'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.startPolling(1)
+bot.startPolling(0)
 bot.launch()
 
