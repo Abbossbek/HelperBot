ed3d7d004bfce193b90c63b990a5078d0b607c49
Abbosbek <39489742+Abbossbek@users.noreply.github.com>
2020-10-14 10:59:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ var token = '1354639458:AAEZlyK3OE-siKDhkXmjFz6AKRJgNTxyQ2k';
 const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf(token)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('??'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('modern', ({ reply }) => reply('Yo'))
+bot.command('hipster', Telegraf.reply('λ'))
 bot.launch()
 
