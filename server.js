aece23a9750e46cb029f11ceb43c7cc1de2fc190
Abbosbek <39489742+Abbossbek@users.noreply.github.com>
2020-10-14 09:49:30 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+
+var token = '1354639458:AAEZlyK3OE-siKDhkXmjFz6AKRJgNTxyQ2k';
+
+const { Telegraf } = require('telegraf')
+
+const bot = new Telegraf(token)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('??'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
