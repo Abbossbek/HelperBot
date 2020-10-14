ba9dfeaa0d257edbe759d1b2383065f05af43313
Abbosbek <39489742+Abbossbek@users.noreply.github.com>
2020-10-14 14:09:20 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function (request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function () {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
 var token = '1354639458:AAEZlyK3OE-siKDhkXmjFz6AKRJgNTxyQ2k';
 
 const { Telegraf } = require('telegraf')
@@ -19,6 +10,16 @@ bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('??'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.startPolling(1,2)
+bot.startPolling()
 bot.launch()
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function (request, response) {
+    var result = 'App is running'
+    bot.launch()
+    response.send(result);
+}).listen(app.get('port'), function () {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
