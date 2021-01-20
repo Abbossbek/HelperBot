658843cd4a5ebf450dd8fb37a58716e4a38cb8d2
Abbosbek <39489742+Abbossbek@users.noreply.github.com>
2021-01-20 19:30:09 +0500

    Update server.js

--- server.js
+++ server.js
@@ -59,20 +59,13 @@ app.get('/', function (request, response) {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
 
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-
  app.get('/task', function (request, response) {
-  	var fs=require('fs');
-	var data=fs.readFileSync('channels.json', 'utf8');
-    response.send(result);
+  	'use strict';
+
+	const fs = require('fs');
+
+	let rawdata = fs.readFileSync('student.json');
+	let student = JSON.parse(rawdata);
+	console.log(student);
+    response.send(rawdata);
 })
